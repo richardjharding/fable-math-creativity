baec444f3851b93005dc7a8fa29264d3de5dbb0f
Richard Harding <richard@sprydondesigns.com>
2017-02-11 00:38:55 +0000

    almost there for first exercise

--- Chapter1/main.fsx
+++ Chapter1/main.fsx
@@ -11,7 +11,7 @@ let width = 800.
 let height = 400.0
 
 type Circle = 
-    {X:float; Y:float;  Scale:float; ScaleSpeed: float; lrSpeed:float;
+    {X:float; Y:float;  Scale:float; ScaleSpeed: float; lrSpeed:float; alpha:float;
     hRad:float; vRad:float ; hRadInc:float; vRadInc:float; lr:float}
 
 let canvas = Browser.document.getElementsByTagName_canvas().[0]
@@ -24,8 +24,9 @@ let drawBg  (ctx:Browser.CanvasRenderingContext2D) canvas =
     ctx.fillStyle <-U3.Case1 "black"
     ctx.fillRect(0.,0.,width,height)
 
-let drawCircle (ctx:Browser.CanvasRenderingContext2D) canvas x y scale = 
-    ctx.save()
+let drawCircle (ctx:Browser.CanvasRenderingContext2D) canvas x y scale alpha = 
+    ctx.save()    
+    ctx.globalAlpha <- alpha
     ctx.beginPath() 
     //ctx.transform(scale,0.,0.,scale,0.,0.)
     ctx.arc (x , y, 15. + scale, 0., 2. * Math.PI, false)
@@ -48,36 +49,65 @@ let vRad () =
 
 let hRadInc = 0.1
 let vRadInc = 0.1
+let scaleMax = 80.
+
 
 
 let MyCircle =
-    {X= width/2.; Y= height/2.; Scale = 1. ; ScaleSpeed = 0.2 ; lrSpeed = lrSpeed();
+    {X= width/2.; Y= height/2.; Scale = 1. ; ScaleSpeed = 0.6 ; lrSpeed = lrSpeed(); alpha = 1.0;
         hRad = hRad(); vRad = vRad(); hRadInc = hRadInc; vRadInc = vRadInc; lr= 0.}
 
+
+let myCircles = 
+    [MyCircle]
+let setAlpha (circle:Circle) = 
+    if(circle.Scale > scaleMax) then         
+        {circle with alpha = circle.alpha - 0.017}
+    else
+        circle
 
 drawBg ctx canvas
 
 drawCircle ctx canvas MyCircle.X MyCircle.Y MyCircle.Scale
 
-let rec loop circle = async {
+let rec loop (circles:Circle list) = async {
     ctx.clearRect(0.,0.,width,height)
     drawBg ctx canvas
-    drawCircle ctx canvas circle.X circle.Y circle.Scale
+
+    let circles = circles 
+                    |> List.map (fun circle -> 
+                    drawCircle ctx canvas circle.X circle.Y circle.Scale circle.alpha
+                
+                    let newX = (width / 2.) + circle.hRad * Math.Sin(circle.lr * Math.PI/180.)
+                    let newY = height /2. + circle.vRad * Math.Cos(circle.lr * Math.PI/180.)
+                    console.log("NewX calculated: " + newX.ToString())
+                    console.log("lrSpeed: " + circle.lrSpeed.ToString())
+                    console.log("lr: " + circle.lr.ToString())
+                    console.log("Scale: " + circle.Scale.ToString())
+                    let newHrad = circle.hRad + circle.hRadInc
+                    let newVrad = circle.vRad + circle.vRadInc
+                    let newLr = circle.lr + circle.lrSpeed
+                    let circle  =  {circle with hRad = newHrad; vRad = newVrad; lr= newLr; X = newX; Y = newY; Scale = circle.Scale + circle.ScaleSpeed}
+                                |> setAlpha
+                    circle    
+                    )     
+                    |> List.filter (fun circle -> 
+                                        circle.alpha > 0.
+                                    )
+                    
+    let f = r.Next(1,1000)/ 10
     
-    let newX = (width / 2.) + circle.hRad * Math.Sin(circle.lr * Math.PI/180.)
-    let newY = height /2. + circle.vRad * Math.Cos(circle.lr * Math.PI/180.)
-    console.log("NewX calculated: " + newX.ToString())
-    console.log("lrSpeed: " + circle.lrSpeed.ToString())
-    console.log("lr: " + circle.lr.ToString())
-    let newHrad = circle.hRad + circle.hRadInc
-    let newVrad = circle.vRad + circle.vRadInc
-    let newLr = circle.lr + circle.lrSpeed
-    let circle  = {circle with hRad = newHrad; vRad = newVrad; lr= newLr; X = newX; Y = newY; Scale = circle.Scale + circle.ScaleSpeed}
+    
     do! Async.Sleep(int (1000/60))
-    return! loop circle
+
+    if(f >= 97)
+    then
+        return! loop  (MyCircle :: circles)
+    else
+        return! loop circles
 
 }
 
-loop MyCircle |> Async.StartImmediate
+loop myCircles |> Async.StartImmediate
 
 
