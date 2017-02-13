6a3a56855e9ee3f200f53f692080e3959d8bc29a
Richard Harding <richard@sprydondesigns.com>
2017-02-13 19:48:31 +0000

    refactor

--- Chapter1/main.fsx
+++ Chapter1/main.fsx
@@ -5,7 +5,6 @@ open Fable.Core
 open Fable.Import
 open Fable.Import.Browser
 
-
 let width = 800.
 let height = 400.0
 
@@ -23,13 +22,11 @@ let drawBg  (ctx:Browser.CanvasRenderingContext2D) canvas =
     ctx.fillStyle <-U3.Case1 "black"
     ctx.fillRect(0.,0.,width,height)
 
-let drawCircle (ctx:Browser.CanvasRenderingContext2D) canvas x y scale alpha = 
+let drawCircle (ctx:Browser.CanvasRenderingContext2D) x y scale alpha = 
     ctx.save()    
     ctx.globalAlpha <- alpha
     ctx.beginPath() 
-    //ctx.transform(scale,0.,0.,scale,0.,0.)
-    ctx.arc (x , y, 15. + scale, 0., 2. * Math.PI, false)
-    console.log("Drawing X: " + x.ToString())
+    ctx.arc (x , y, 15. + scale, 0., 2. * Math.PI, false)    
     ctx.fillStyle <- U3.Case1 "red"
     ctx.fill()
     ctx.stroke()
@@ -48,63 +45,51 @@ let vRad () =
 
 let hRadInc = 0.1
 let vRadInc = 0.1
-let scaleMax = 80.
+let scaleMax = 90.
 
-
-
-let MyCircle =
+let createCircle () =
     {X= width/2.; Y= height/2.; Scale = 1. ; ScaleSpeed = 0.6 ; lrSpeed = lrSpeed(); alpha = 1.0;
         hRad = hRad(); vRad = vRad(); hRadInc = hRadInc; vRadInc = vRadInc; lr= 0.}
 
-
-let myCircles = 
-    [MyCircle]
 let setAlpha (circle:Circle) = 
     if(circle.Scale > scaleMax) then         
-        {circle with alpha = circle.alpha - 0.017}
+        {circle with alpha = circle.alpha - 0.016}
     else
         circle
 
-drawBg ctx canvas
+let calculateNewCoords (circle:Circle) = 
+    let newX = (width / 2.) + circle.hRad * Math.Sin(circle.lr * Math.PI/180.)
+    let newY = height /2. + circle.vRad * Math.Cos(circle.lr * Math.PI/180.)  
+    newX, newY
 
-drawCircle ctx canvas MyCircle.X MyCircle.Y MyCircle.Scale
+let updateCircle (circle:Circle) =
+    let newX,newY = calculateNewCoords circle                   
+    let newHrad = circle.hRad + circle.hRadInc
+    let newVrad = circle.vRad + circle.vRadInc
+    let newLr = circle.lr + circle.lrSpeed
+    {circle with hRad = newHrad; vRad = newVrad; lr= newLr; X = newX; Y = newY; Scale = circle.Scale + circle.ScaleSpeed}
+    
+let myCircles = 
+    [createCircle()]
 
 let rec loop (circles:Circle list) = async {
     ctx.clearRect(0.,0.,width,height)
     drawBg ctx canvas
 
+    circles |> List.iter (fun c -> drawCircle ctx c.X c.Y c.Scale c.alpha)
+
     let circles = circles 
-                    |> List.map (fun circle -> 
-                    drawCircle ctx canvas circle.X circle.Y circle.Scale circle.alpha
-                
-                    let newX = (width / 2.) + circle.hRad * Math.Sin(circle.lr * Math.PI/180.)
-                    let newY = height /2. + circle.vRad * Math.Cos(circle.lr * Math.PI/180.)
-                    console.log("NewX calculated: " + newX.ToString())
-                    console.log("lrSpeed: " + circle.lrSpeed.ToString())
-                    console.log("lr: " + circle.lr.ToString())
-                    console.log("Scale: " + circle.Scale.ToString())
-                    let newHrad = circle.hRad + circle.hRadInc
-                    let newVrad = circle.vRad + circle.vRadInc
-                    let newLr = circle.lr + circle.lrSpeed
-                    let circle  =  {circle with hRad = newHrad; vRad = newVrad; lr= newLr; X = newX; Y = newY; Scale = circle.Scale + circle.ScaleSpeed}
-                                |> setAlpha
-                    circle    
-                    )     
-                    |> List.filter (fun circle -> 
-                                        circle.alpha > 0.
-                                    )
-                    
-    let f = r.Next(1,1000)/ 10
-    
+                    |> List.map (fun circle -> circle |> (updateCircle >> setAlpha))     
+                    |> List.filter (fun circle -> circle.alpha > 0.)
     
     do! Async.Sleep(int (1000/60))
 
-    if(f >= 97)
+    let f = r.Next(1,1000)/ 10 
+    if(f >= 98)
     then
-        return! loop  (MyCircle :: circles)
+        return! loop  (createCircle() :: circles)
     else
         return! loop circles
-
 }
 
 loop myCircles |> Async.StartImmediate
